3707f05d26669573388cf7138bf25a3ab94b36a3
James Hallberg <Grebllah81788@gmail.com>
2024-01-30 13:00:15 -0500

    organizing bears lab complete

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ("Mr. Chocolate", 4, "M", "Brown", "Docile");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ("Rowdy", 7, "F", "Tan", "Extra Rowdy");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ("Tabitha", 10, "F", "Black", "Hostile");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ("Sergeant Brown", 9, "M", "Brown", "Gentle");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ("Melissa", 5, "F", "Brown", "The Best");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ("Grinch", 6, "F", "Black", "Mischievous");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES ("Wendy", 14, "F", "White", "Slow");
+INSERT INTO bears (name, age, sex, color, temperament) VALUES (NULL, 4, "M", "Tan", "Aggressive");
